cca78c875dbe1112f67666e2892f4362e61ac2ec
Ray Brown-Amory <r.brown-amory@sky.com>
2024-03-29 16:45:37 +0000

    Add project files.

---
+++ GloboTicket.Admin.Moble/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GloboTicket.Admin.Moble
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GloboTicket.Admin.Moble/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace GloboTicket.Admin.Moble
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
